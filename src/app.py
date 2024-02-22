5231483017b271a18c91e5cd5074d663ea65c0e8
Milan Sastry <ms1438@princeton.edu>
2024-02-21 22:27:13 -0500

    penis

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+if __name__ == '__main__':
+    app.run(host = 'localhost', debug=True)
